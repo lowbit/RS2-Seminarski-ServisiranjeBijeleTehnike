638b6e70a85042b5fdf9f7b01c6e932314c6e0a2
Rijad Spahic <spaha92@gmail.com>
2021-03-16 11:47:28 +0100

    Added xamarin forms mobile app and login page.

---
+++ SBT.Mobile/SBT.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SBT.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SBT.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ SBT.Mobile/SBT.Mobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SBT.Mobile.Models;
+using SBT.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SBT.Mobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
